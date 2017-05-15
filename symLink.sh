44c7f2aff98ede7adb57d50a049cc38c46a1d42a
Jared Steiner <steiner.jared199@gmail.com>
2017-05-15 14:12:39 +1000

    Added bash file setup

---
+++ symLink.sh
@@ -0,0 +1,7 @@
+ln -s .dotfiles/atom/.atom ~/
+ln -s .dotfiles/term/.bash_profile ~/.bash_profile
+ln -s .dotfiles/zsh/.oh-my-zsh ~/
+ln -s .dotfiles/zsh/.zshrc ~/.zshrc
+ln -s .dotfiles/vim/.vimrc ~/.vimrc
+ln -s .dotfiles/term/.aliases ~/.aliases
+echo "Finished Linking"
