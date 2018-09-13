dfc97b3889be6d559e377bdd603d621160288051
Jared Steiner <jared.steiner@jared.steiner.macbook>
2018-09-12 21:41:05 -0700

    Updated and refreshed

--- baby_steps.sh
+++ baby_steps.sh
@@ -1,18 +1,41 @@
 #!/bin/bash
 
-# Downloading Dotfiles
-git clone git@github.com:jayman888/dotfiles.git ~/.dotfiles
-cd ~/.dotfiles && ./symLink.sh
+# Setting up SSH Keys
+mkdir -p ~/.ssh
+pushd ~/.ssh
+#touch github_rsa cse_rsa bitbucket_rsa
+#chmod 400 *_rsa
+#touch github_rsa.pub cse_rsa.pub bitbucket_rsa.pub
+#touch 644 *_rsa.pub
+echo "[ACTION]: Open a new terminal and fill ~/.ssh/*_rsa keys from dashlane"
+printf 'Enter anything to continue script?... '
+read -r break
+popd
+echo "[ACTION]: Authorise adding key to CSE machines!"
+ssh-copy-id -i ~/.ssh/cse_rsa.pub z5153405@wagner.cse.unsw.edu.au
 
-# Downloading Oh My Zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+# Downloading Dotfiles
+echo "[DOWNLOAD]: Personal Dotfiles from github"
+git clone git@github.com:jayman888/dotfiles.git ~/.dotfiles
+echo " - Symlinking"
+pushd ~/.dotfiles
+./symLink.sh
+popd
 
 # Pathogen for Vim
 mkdir -p ~/.vim/autoload ~/.vim/bundle
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
 # Installing Brew for Mac
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+# - Flimsy check if mac
+if -d /Applications; then
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+else
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
+fi
+
+# Installing pytho3
+brew install python3
 
 # Installing Chunkwm
 brew tap crisidev/homebrew-chunkwm
@@ -22,3 +45,7 @@ brew install --HEAD --with-logging  koekeishiya/formulae/skhd
 echo "STARTING CHUNK"
 brew services start chunkwm
 brew services start skhd
+
+# Downloading Oh My Zsh (Should be run LAST, opens zsh prompt halting everything else)
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
