d028702625ee00bf24003a39fad299a49c24cfa7
Jared Steiner <steiner.jared1996@gmail.com>
2019-03-25 11:03:00 +1100

    Updated to include more apps

--- baby_steps.sh
+++ baby_steps.sh
@@ -12,7 +12,7 @@ printf 'Enter anything to continue script?... '
 read -r break
 popd
 echo "[ACTION]: Authorise adding key to CSE machines!"
-ssh-copy-id -i ~/.ssh/cse_rsa.pub z5153405@wagner.cse.unsw.edu.au
+#ssh-copy-id -i ~/.ssh/cse_rsa.pub z5153405@wagner.cse.unsw.edu.au
 
 # Downloading Dotfiles
 echo "[DOWNLOAD]: Personal Dotfiles from github"
@@ -23,34 +23,37 @@ pushd ~/.dotfiles
 popd
 
 # Pathogen for Vim
+echo "[DOWNLOAD]: Pathogen for Vim"
 mkdir -p ~/.vim/autoload ~/.vim/bundle
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 # Vim Undo dir
 mkdir -p ~/.vim/undo-dir
 
 # Installing Brew for Mac
+echo "[DOWNLOAD]: Brew for Mac/Linux"
 # - Flimsy check if mac
 if -d /Applications; then
-    mkdir -p ~/Screenshots && defaults write com.apple.screencapture location ~/Screenshots
     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    mkdir -p ~/Screenshots && defaults write com.apple.screencapture location ~/Screenshots
 else
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
 fi
 
-# Installing pytho3
+# Installing python3
 brew install python3
 
 # Installing Chunkwm
-#brew tap crisidev/homebrew-chunkwm
-#brew install --HEAD --with-tmp-logging chunkwm
-#brew install --HEAD --with-logging  koekeishiya/formulae/skhd
+brew tap crisidev/homebrew-chunkwm
+brew install --HEAD --with-tmp-logging chunkwm
+brew install --HEAD --with-logging  koekeishiya/formulae/skhd
 
 #echo "STARTING CHUNK"
-#brew services start chunkwm
-#brew services start skhd
+brew services start chunkwm
+brew services start skhd
 
-
-
+# Macapps.link
+# - Chrome, Alfred, iTerm
+curl -s 'https://macapps.link/en/chrome-alfred-iterm' | sh
 
 # Downloading Oh My Zsh (Should be run LAST, opens zsh prompt halting everything else)
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
